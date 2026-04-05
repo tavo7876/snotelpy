e9b052e598a6e65ea1cf3a60a57e8599da32ba1f
tavo7876 <tavo7876@colorado.edu>
2026-04-04 21:03:36 -0600

    added functionability with swe_timeseries

---
+++ src/snotelpy/plot.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import xarray as xr
+
+
+
+def swe_timeseries(ds, element = "WTEQ", ShowPlot = True):
+    '''
+    ------
+    enter a ds
+    '''
+
+    fig, ax = plt.subplots()
+    for station in ds.station.values:
+        ds[element.strip().upper()].sel(station=station).plot(ax=ax,label=station)
+        
+    ax.legend()
+    ax.grid(True, alpha = 0.3)
+    ax.set_title(f"{element} time series")
+    ax.set_xlabel('Time')
+    plt.tight_layout()
+    if ShowPlot:
+        plt.show() 
+        
+    return ax
+    
+  
+        
+        
+        
+    
