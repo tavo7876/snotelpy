6578e67e2cfd37be43346f648fc11878a5044db4
tavo7876 <tavo7876@colorado.edu>
2026-04-06 22:27:59 -0600

    refactor element_timeseries to allow optional ax parameter for better plot handling

--- src/snotelpy/plot.py
+++ src/snotelpy/plot.py
@@ -3,13 +3,14 @@ import xarray as xr
 
 
 
-def element_timeseries(ds, element = "WTEQ", ShowPlot = True):
+def element_timeseries(ds, element = "WTEQ", ShowPlot = True, ax=None):
     '''
     ------
     enter a ds
     '''
 
-    fig, ax = plt.subplots()
+    if ax is None:
+        fig, ax = plt.subplots()
     for station in ds.station.values:
         ds[element.strip().upper()].sel(station=station).plot(ax=ax,label=station,marker = 'o')
         
@@ -17,7 +18,8 @@ def element_timeseries(ds, element = "WTEQ", ShowPlot = True):
     ax.grid(True, alpha = 0.3)
     ax.set_title(f"{element} time series")
     ax.set_xlabel('Time')
-    plt.tight_layout()
+    plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
+    
     if ShowPlot:
         plt.show() 
         
